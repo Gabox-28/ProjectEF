9cc809247e162443ab7ae477c3b0c33740b3ae8d
Gabriel Mayorga <gabrielmayorgate@gmail.com>
2023-03-26 02:02:49 -0600

    Mapade de modelos con sus atributos y base de datos en memoria

---
+++ Models/Tarea.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectEF.Models;
+
+public class Tarea
+{
+    [Key]
+    public Guid TareaId { get; set; }
+    
+    [ForeignKey("CategoriaId")]
+    public  Guid CategoriaId { get; set; }
+    
+    [Required]
+    [MaxLength(200)]
+    public string Titulo { get; set; }
+    public string Descripcion { get; set; }
+    public Prioridad PrioridadTarea { get; set; }
+    public DateTime FechaCreacion { get; set; }
+    
+    //Permite traer la informacion de la categoria asociada con la tarea
+    public virtual Categoria Categoria { get; set; }
+    
+    [NotMapped]
+    public string resumen { get; set; }
+}
+
+public enum Prioridad
+{
+    Baja,
+    Media,
+    Alta
+}
